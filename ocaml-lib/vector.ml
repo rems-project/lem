c401e2d7556f1b37c6efc8ac89538b2dcdd7f16b
Thomas Tuerk <tt291@cl.cam.ac.uk>
2013-10-24 05:21:31 +0100

    port vector ocaml-lib to big_int

--- ocaml-lib/vector.ml
+++ ocaml-lib/vector.ml
@@ -1,6 +1,7 @@
 open Nat_num
 
 type 'a vector = Vector of 'a array
+type nat = Big_int.big_int 
 
 let vconcat (Vector a) (Vector b) = Vector(Array.append a b)
 
@@ -20,16 +21,16 @@ let vmapacc f (Vector a) base =
   let vls,b = mapacc (Array.to_list a) base in
   Vector(Array.of_list vls),b
 
-let vmapi f (Vector a) = Vector(Array.mapi f a)
+let vmapi f (Vector a) = Vector(Array.mapi (fun i e -> f (Big_int.big_int_of_int i) e) a)
 
-let extend default size (Vector a) = Vector(Array.append (Array.make size default) a)
+let extend default size (Vector a) = Vector(Array.append (Array.make (Big_int.int_of_big_int size) default) a)
 
 let duplicate (Vector a) = Vector(Array.append a (Array.copy a))
 
-let vlength (Vector a) = Array.length a
+let vlength (Vector a) = Big_int.big_int_of_int (Array.length a)
 
-let vector_access n (Vector a) = a.(n)
+let vector_access n (Vector a) = a.(Big_int.int_of_big_int n)
 
-let vector_slice n1 n2 (Vector a) = Vector(Array.sub a n1 n2)
+let vector_slice n1 n2 (Vector a) = Vector(Array.sub a (Big_int.int_of_big_int n1) (Big_int.int_of_big_int n2))
 
 let make_vector vs l = Vector(Array.of_list vs)
--- ocaml-lib/vector.mli
+++ ocaml-lib/vector.mli
@@ -1,6 +1,7 @@
 open Nat_num
 
 type 'a vector = Vector of  'a array
+type nat = Big_int.big_int 
 
 val vconcat : 'a vector -> 'a vector -> 'a vector 
 
@@ -12,17 +13,17 @@ val vzip : 'a vector -> 'b vector -> ('a * 'b) vector
 
 val vmapacc : ('a -> 'c -> ('b * 'c)) -> 'a vector -> 'c -> ('b vector) * 'c
 
-val vmapi : (num -> 'a -> 'b) -> 'a vector -> 'b vector
+val vmapi : (nat -> 'a -> 'b) -> 'a vector -> 'b vector
 
-val extend : 'a -> num -> 'a vector -> 'a vector
+val extend : 'a -> nat -> 'a vector -> 'a vector
 
 val duplicate : 'a vector -> 'a vector
 
-val vlength : 'a vector -> num
+val vlength : 'a vector -> nat
 
-val vector_access : num ->'a vector -> 'a
+val vector_access : nat ->'a vector -> 'a
 
-val vector_slice : num -> num ->'a vector -> 'a vector
+val vector_slice : nat -> nat ->'a vector -> 'a vector
 
-val make_vector : 'a list -> num -> 'a vector
+val make_vector : 'a list -> nat -> 'a vector
 
