0a1d66ca383f9e082db76b8a566f613e36ec5fed
Thomas Tuerk <thomas@tuerk-brechen.de>
2014-01-27 11:44:08 +0000

    implemented int32 for OCaml
    implemented type int32 with proper type "Int32.t" in Ocaml. Added
    bindings in "num.lem" and "word.lem". Additionally some tests were
    added.

--- ocaml-lib/nat_num.ml
+++ ocaml-lib/nat_num.ml
@@ -17,17 +17,19 @@ let natural_monus x y =
 let nat_pred x = nat_monus x 1
 let natural_pred x = natural_monus x Big_int.unit_big_int
 
-let nat_pow b e = 
-  let rec aux a b e =
-     if e = 1 then (a * b) else
+let gen_pow (one : 'a) (mul : 'a -> 'a -> 'a) (b : 'a) (e : int) : 'a = 
+  let rec aux (a : 'a) (b : 'a) (e : int) =
+     if e = 1 then (mul a b) else
       let e' = e / 2 in
-      let a' = (if (e mod 2) = 0 then a else (a * b)) in
-      aux a' (b * b) e'
+      let a' = (if (e mod 2) = 0 then a else mul a b) in
+      aux a' (mul b b) e'
   in     
   if e < 0 then raise (Failure "negative exponent") else 
-  if (e = 0) then 1 else aux 1 b e
+  if (e = 0) then one else aux one b e
 
+let nat_pow b e = gen_pow 1 (fun a b -> a * b) b e;;
 let int_pow b e = nat_pow b e;;
+let int32_pow (b : int32) e = gen_pow Int32.one Int32.mul b e
  
 let int_mod i n = 
   let r = i mod n in
@@ -36,3 +38,12 @@ let int_mod i n =
 let int_div i n = 
   let r = i / n in
   if (i mod n < 0) then r - 1 else r
+
+let int32_mod i n = 
+  let r = Int32.rem i n in
+  if (r < Int32.zero) then Int32.add r n else r
+
+let int32_div i n = 
+  let r = Int32.div i n in
+  if (Int32.rem i n < Int32.zero) then Int32.pred r else r
+
--- ocaml-lib/nat_num.mli
+++ ocaml-lib/nat_num.mli
@@ -4,9 +4,12 @@ type natural = Big_int.big_int
 val natural_monus : natural -> natural -> natural
 val natural_pred : natural -> natural
 
-val nat_pred : nat -> nat
+val nat_pred  : nat -> nat
 val nat_monus : nat -> nat -> nat
-val nat_pow : nat -> nat -> nat
-val int_pow : int -> nat -> int
-val int_div : int -> int -> int
-val int_mod : int -> int -> int
+val nat_pow   : nat -> nat -> nat
+val int_pow   : int -> nat -> int
+val int32_pow : Int32.t -> nat -> Int32.t
+val int_div   : int -> int -> int
+val int32_div : Int32.t -> Int32.t -> Int32.t
+val int_mod   : int -> int -> int
+val int32_mod : Int32.t -> Int32.t -> Int32.t
