0646dfa68b15b4e7db3bc899225ff8211b5e15a2
Dominic Mulligan <dominic.p.mulligan@googlemail.com>
2014-02-13 11:09:23 +0000

    Resolve issue 115
    * Resolve issue 115 with broken bindings to character type in the Coq
    backend.  Also added missing functionality in the Coq string library.

--- coq-lib/coqharness.v
+++ coq-lib/coqharness.v
@@ -1,6 +1,7 @@
 Require Import Coq.ZArith.BinInt.
 Require Import Coq.ZArith.Zorder.
 Require Import ClassicalDescription.
+Require Import Coq.Strings.Ascii.
 Require Import Coq.Strings.String.
 
 (* Logic *)
@@ -48,6 +49,28 @@ Fixpoint list_member_by
           else
             list_member_by elteq e xs
     end.
+
+Program Fixpoint list_head
+  {elt: Type} (l: list elt) (p: 0 < List.length l): elt :=
+    match l return 0 < List.length l -> elt with
+      | [] => fun absurd => _
+      | x::xs => fun irrelevant => x
+    end p.
+  Obligation 1.
+    destruct (Coq.Arith.Lt.lt_irrefl 0).
+    apply absurd.
+Qed.
+
+Program Fixpoint list_tail
+  {elt: Type} (l: list elt) (p: 0 < List.length l): list elt :=
+    match l return 0 < List.length l -> list elt with
+      | [] => fun absurd => _
+      | x::xs => fun irrelevant => xs
+    end p.
+  Obligation 1.
+    destruct (Coq.Arith.Lt.lt_irrefl 0).
+    apply absurd.
+Qed.
 
 (* Comparisons *)
 
@@ -400,6 +423,30 @@ Definition fmap_domain_by
 Definition fmap_range_by
   {k v: Type} (vord: v -> v -> ordering) (map: fmap k v): set v :=
     List.map (@snd k v) map.
+
+(* Strings *)
+
+Fixpoint string_to_char_list
+  (s: string): list ascii :=
+    match s with
+      | EmptyString => []
+      | String hd tl => hd::string_to_char_list tl
+    end.
+
+Fixpoint string_from_char_list
+  (chars: list ascii): string :=
+    match chars with
+      | [] => EmptyString
+      | hd::tl => String hd (string_from_char_list tl)
+    end.
+
+Fixpoint string_make_string
+  (m: nat) (char: ascii): string :=
+    match m with
+      | 0 => EmptyString
+      | S m' => String char (string_make_string m' char)
+    end.
+
 
 (* Default values for incomplete pattern matching. *)
 
