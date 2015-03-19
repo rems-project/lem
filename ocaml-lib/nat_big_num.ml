0bfbb1876def6e69af7a19121dc03a783d59fbb1
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2015-03-19 13:26:25 +0000

    changed all ocaml library references to Big_int to Nat_big_num, an abstraction layer for big integer types, so that we can swap out Big_int with zarith based library easily by just changing bindings in nat_big_num.ml

--- ocaml-lib/nat_big_num.ml
+++ ocaml-lib/nat_big_num.ml
@@ -1,18 +1,52 @@
 module BI = Big_int
 
 type num = BI.big_int
-let (<) = BI.lt_big_int
-let (<=) = BI.le_big_int
-let (>) = BI.gt_big_int
-let (>=) = BI.ge_big_int
-let (+) = BI.add_big_int
-let (-) x y =
-  let d = BI.sub_big_int x y in
-    if d < BI.zero_big_int then
-      BI.zero_big_int
-    else
-      d
-let ( * ) = BI.mult_big_int
-let (/) = BI.div_big_int
-let (mod) = BI.mod_big_int
-let string_of_num = BI.string_of_big_int
+
+let succ = BI.succ_big_int
+let pred = BI.pred_big_int
+let pred_nat p =
+  if BI.eq_big_int p BI.zero_big_int then
+    BI.zero_big_int
+  else
+    BI.pred_big_int p
+let negate = BI.minus_big_int
+
+let add = BI.add_big_int
+let sub = BI.sub_big_int
+let sub_nat left right =
+  if BI.gt_big_int right left then
+    BI.zero_big_int
+  else
+    BI.sub_big_int left right
+let mul = BI.mult_big_int
+let pow_int = BI.power_big_int_positive_int
+
+let abs = BI.abs_big_int
+let modulus = BI.mod_big_int
+let div = BI.div_big_int
+
+let min = BI.min_big_int
+let max = BI.max_big_int
+
+let less = BI.lt_big_int
+let greater = BI.gt_big_int
+let less_equal = BI.le_big_int
+let greater_equal = BI.ge_big_int
+
+let compare = BI.compare_big_int
+let equal = BI.eq_big_int
+
+let shift_left = BI.shift_left_big_int
+let shift_right = BI.shift_right_big_int
+let bitwise_or = BI.or_big_int
+let bitwise_xor  = BI.xor_big_int
+let bitwise_and = BI.and_big_int
+
+let of_int = BI.big_int_of_int
+let to_int = BI.int_of_big_int
+let of_int32 = BI.big_int_of_int32
+let to_int32 = BI.int32_of_big_int
+let of_int64 = BI.big_int_of_int64
+let to_int64 = BI.int64_of_big_int
+
+let to_string = BI.string_of_big_int
--- ocaml-lib/nat_big_num.mli
+++ ocaml-lib/nat_big_num.mli
@@ -1,11 +1,43 @@
 type num
-val (<) : num -> num -> bool
-val (<=) : num -> num -> bool
-val (>) : num -> num -> bool
-val (>=) : num -> num -> bool
-val (+) : num -> num -> num
-val (-) : num -> num -> num
-val ( * ) : num -> num -> num
-val (/) : num -> num -> num
-val (mod) : num -> num -> num
-val string_of_num : num -> string
+
+val succ : num -> num
+val pred : num -> num
+val pred_nat : num -> num
+val negate : num -> num
+
+val add : num -> num -> num
+val sub : num -> num -> num
+val sub_nat : num -> num -> num
+val div : num -> num -> num
+val mul : num -> num -> num
+val pow_int : num -> int -> num
+
+val abs : num -> num
+val modulus : num -> num -> num
+
+val min : num -> num -> num
+val max : num -> num -> num
+
+val less : num -> num -> bool
+val greater : num -> num -> bool
+val less_equal : num -> num -> bool
+val greater_equal : num -> num -> bool
+
+val compare : num -> num -> int
+val equal : num -> num -> bool
+
+val bitwise_or : num -> num -> num
+val bitwise_and : num -> num -> num
+val bitwise_xor : num -> num -> num
+val shift_left : num -> int -> num
+val shift_right : num -> int -> num
+
+val of_int : int -> num
+val of_int32 : Int32.t -> num
+val of_int64 : Int64.t -> num
+
+val to_int : num -> int
+val to_int32 : num -> Int32.t
+val to_int64 : num -> Int64.t
+
+val to_string : num -> string
