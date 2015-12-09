ce327c69d734dcdbbd8d450d525c0b5a941eb288
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2015-12-09 16:40:09 +0000

    Bumped up the fresh name limit for Christopher

--- src/name.ml
+++ src/name.ml
@@ -126,9 +126,9 @@ let fresh_start start s ok =
           if ok (from_rope x) then
             x 
           else
-            f 3000 (* give up after 1000 tries *) 0
+            f 5000 (* give up after 1000 tries *) 0
       | Some(i) ->
-          f 3000 i
+          f 5000 i
 
 let fresh s ok = from_rope (fresh_start None s ok)
 
