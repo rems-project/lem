51c9ed9b751b092cfa077f24787e9a188f4e929c
Shaked Flur <fshaked@gmail.com>
2017-12-13 16:06:22 +0000

    tests use zarith package now

--- library/run-ocaml-tests.sh
+++ library/run-ocaml-tests.sh
@@ -7,16 +7,11 @@ cd $dir
 rm -f ocaml-lib
 ln -s ../../ocaml-lib .
 
-# OCaml foolishly provides "its own" LD_LIBRARY_PATH, but the vanilla one
-# also takes effect if the OCaml one is not set. So we have to munge both.
-export LD_LIBRARY_PATH="$(readlink -f ocaml-lib)"/dependencies/zarith/:"${LD_LIBRARY_PATH}"
-export CAML_LD_LIBRARY_PATH="$(readlink -f ocaml-lib)"/dependencies/zarith/:"${CAML_LD_LIBRARY_PATH}"
-
 for file in *Auxiliary.ml
 do
   echo $file
   file_nat=${file%.ml}.native
-  ocamlfind ocamlc -o ${file_nat} -I ocaml-lib/dependencies/zarith/ -I ocaml-lib zarith.cma nums.cma extract.cma ${file}
+  ocamlfind ocamlc -package zarith -linkpkg -o ${file_nat} -I ocaml-lib/_build_zarith nums.cma extract.cma ${file}
 done 
 
 for file in *.native 
